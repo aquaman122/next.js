24474fb454235bc1d4ddcd102d95548e2c163fcb
aquaman <swj981128@gmail.com>
2023-04-23 17:29:42 +0900

    0

--- coding-apple-nextjs/fresh/app/page.js
+++ coding-apple-nextjs/fresh/app/page.js
@@ -3,11 +3,7 @@ export default function Home() {
   let link = 'http://google.com'
   return (
     <>
-      <div>
-        <h4 className="title">애플 후레시</h4>
-        <p className="title">by dev {name}</p>
-        <a href={link}>링크</a>
-      </div>
+      
     </>
   )
 }
