c8c58b72d8084abe68535429ea044173fef0bd5b
aquaman <swj981128@gmail.com>
2023-04-28 12:42:21 +0900

    about

---
+++ imbb-clone/src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+      <div className='max-w-6xl mx-auto space-y-4'>
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+          <p>
+          Used by some of the world's largest companies,
+           Next.js enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.
+          “My favorite UX feedback from customers is: "How is the app so fast?" Because we’ve built on Next.js and Vercel since day one, our pages load in an instant, which is important when it comes to mission-critical software.”
+          </p>
+          <p>
+          Used by some of the world's largest companies,
+           Next.js enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.
+          “My favorite UX feedback from customers is: "How is the app so fast?" Because we’ve built on Next.js and Vercel since day one, our pages load in an instant, which is important when it comes to mission-critical software.”
+          </p>
+          <p>
+          Used by some of the world's largest companies,
+           Next.js enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.
+          “My favorite UX feedback from customers is: "How is the app so fast?" Because we’ve built on Next.js and Vercel since day one, our pages load in an instant, which is important when it comes to mission-critical software.”
+          </p>
+      </div>
+    </>
+  )
+}
